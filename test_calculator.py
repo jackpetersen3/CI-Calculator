b1b0d17fc74b32ae391dfaeeb94d1da6c42aaeb3
jackpetersen3 <peterja7@oregonstate.edu>
2021-06-04 12:30:53 -0700

    multiply functionality and test added

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,7 @@ class TestCalculatorApp:
 
     def test_subtract(self):
         assert 2 == calculator.subtract(5, 3)
+
+    def test_multiply(self):
+        assert 10 == calculator.multiply(5, 2)
+
